d1ef9a52c08464b3273498b61a83c2e05f2d5c3b
Eugene Kalnyk <eugene.kalnyk@gmail.com>
2024-05-27 07:33:33 +0300

    final files

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import requests
 url = "https://churn-project-mzk3f6u53a-lm.a.run.app/predict"
 
 params = {
-    'q':"I7T7PCX8QD"
+    'q':"VNQUDIIEXS"
 }
 
 answer = requests.get(url, params=params)
